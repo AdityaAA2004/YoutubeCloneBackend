df06f05c79cd24b392db90c6128573db1decba6d
AdityaAA2004 <anandad1@msu.edu>
2025-10-25 19:34:06 -0400

    Try deploying again

--- src/main/java/com/aditya/youtube_clone/YoutubeCloneApplication.java
+++ src/main/java/com/aditya/youtube_clone/YoutubeCloneApplication.java
@@ -1,16 +1,18 @@
 package com.aditya.youtube_clone;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @Slf4j
 @SpringBootApplication
 public class YoutubeCloneApplication {
-
+    @Value("${spring.data.mongodb.uri}")
+    private static String myApplicationProperty;
 	public static void main(String[] args) {
         log.info("Starting Youtube Clone Application with DB URL from secrets: {} ...", System.getenv("DB_URL"));
-        log.info("The DB url is: {}", System.getenv("spring.data.mongodb.uri"));
+        log.info("The DB url is: {}", myApplicationProperty);
 		SpringApplication.run(YoutubeCloneApplication.class, args);
 	}
 
